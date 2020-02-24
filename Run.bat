f3297cadff7839ca1314364c66e52b9bdb733822
libaojun <libaojunqd@foxmail.com>
2020-02-24 17:24:23 +0800

    脚本更新

--- Run.bat
+++ Run.bat
@@ -21,14 +21,13 @@ echo.
 cd /d "%~dp0"
 
 set currentPath=%cd%
-echo %currentPath%
 
 echo 请输入qmake.exe所在路径，例如：C:\Qt\Qt5.4.2\5.4\msvc2013_64_opengl\bin\：
 echo.
 set /p qmakeDir=请输入:
 echo.
 set qmakePath=%qmakeDir%\qmake.exe
-echo %qmakePath%
+echo.
 
 
 echo 请输入Visual Studio 2013安装目录，例如：C:\Program Files (x86)\Microsoft Visual Studio 12.0\：
@@ -36,7 +35,8 @@ echo (该目录下存在VC、Common7等文件夹)
 echo.
 set /p VSDir=请输入:
 echo.
-echo "%VSDir%"
+echo 正在生成批处理脚本...
+echo.
 
 :Replace
 set codePath=%currentPath%\Code
@@ -86,14 +86,70 @@ echo start "%VSDir%\Common7\IDE\devenv.exe" FastCAE.sln >> %codePath%\Run_MSVC.b
 echo. >> %codePath%\Run_MSVC.bat
 
 echo 批处理脚本信息更新完毕！
+echo.
 echo 接下来将生成开发环境。。。
 pause
 
 cd %codePath%
-
 call "%codePath%\Copy_DLLs.bat"
+cd %codePath%
 call "%codePath%\Copy_Pys.bat"
+cd %codePath%
 call "%codePath%\Create_X64_Project.bat"
+
+cd %currentPath%
+copy /y "%qmakeDir%\Qt5Widgetsd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Guid.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Cored.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Xmld.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5PrintSupportd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5OpenGLd.dll" ".\output\bin_d"
+
+copy /y "%qmakeDir%\Qt5WebKitWidgetsd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Networkd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5WebKitd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Sensorsd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5MultimediaWidgetsd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Multimediad.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Positioningd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5WebChanneld.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5PrintSupport.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Quickd.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Qmld.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\Qt5Sqld.dll" ".\output\bin_d"
+
+copy /y "%qmakeDir%\icuin53.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\icuuc53.dll" ".\output\bin_d"
+copy /y "%qmakeDir%\icudt53.dll" ".\output\bin_d"
+cd .\output\bin_d\
+mkdir .\platforms\
+copy /y "%qmakeDir%\..\plugins\platforms\" ".\platforms\"
+
+cd %currentPath%
+copy /y "%qmakeDir%\Qt5Widgets.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Gui.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Core.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Xml.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5OpenGL.dll" ".\output\bin"
+		 %qmakeDir%
+copy /y "%qmakeDir%\Qt5WebKitWidgets.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Network.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5WebKit.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Sensors.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5MultimediaWidgets.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Multimedia.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Positioning.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5WebChannel.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5PrintSupport.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Quick.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Qml.dll" ".\output\bin"
+copy /y "%qmakeDir%\Qt5Sql.dll" ".\output\bin"
+copy /y "%qmakeDir%\icuin53.dll" ".\output\bin"
+copy /y "%qmakeDir%\icuuc53.dll" ".\output\bin"
+copy /y "%qmakeDir%\icudt53.dll" ".\output\bin"
+cd .\output\bin\
+mkdir .\platforms\
+copy /y "%qmakeDir%\..\plugins\platforms\" ".\platforms\"
 
 echo 下面将启动Visual Studio 2013
 pause
